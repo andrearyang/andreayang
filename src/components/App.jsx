42656cc2e15111a90da262e62a1b57a7a4ea6e98
Andrea Yang <andreayang64@gmail.com>
2024-09-01 20:45:28 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          This is a basic React setup.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
