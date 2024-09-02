53e0902efc246ac13e501774f795bce37faf4d1b
Andrea Yang <andreayang64@gmail.com>
2024-09-01 21:19:03 -0700

    add text

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './globals.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Andrea Yang</h1>
+        <p>
+         Hello! <br /> <br /> I’m Andrea, a 16 year old hacker from California. I like computers and cells. <br /> <br /> Accessible education is really important to me. I also strongly believe in pursuing math and making as an art. I'm currently building related projects and research.
+        </p>
+        <p>
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
