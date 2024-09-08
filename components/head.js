82ec26bdff13dbbcf3040ccb99dc016a89154431
Andrea Yang <andreayang64@gmail.com>
2024-09-08 00:12:42 -0700

    icon

--- components/head.js
+++ components/head.js
@@ -5,9 +5,9 @@ export default function HeadObject({children}) {
     const description = "Andrea Yang's Personal Site";
     const searchBarColor = "#ffffff"; // This is your Safari 15 Search Bar Color in Light Mode
     //const darkSearchBarColor = "#000000"; // This is your Safari 15 Search Bar Color in Dark Mode (optional)
-    const keywords = "put some fun keywords here";
+    const keywords = "andrea";
     const author = "Andrea Yang";
-    const twitter = "@sarthaktexas";
+    const twitter = "@idkidk";
     const url = "http://localhost:3000"; // This is your og:url or domain (optional but recommended)
     //const image = "/ogimage.png"; // This is your OpenGraph image
     return (
@@ -20,7 +20,7 @@ export default function HeadObject({children}) {
             <meta name="keywords" content={keywords} />
             <meta name="author" content={author} />
             <meta name="theme-color" content={searchBarColor} media="(prefers-color-scheme: light)" />
-            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😋</text></svg>" />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
             {/* <meta name="theme-color" content={darkSearchBarColor} media="(prefers-color-scheme: dark)" /> */}
             {url ? <meta property="og:url" content={url} /> : ''}
             <meta property="og:type" content="website" />
