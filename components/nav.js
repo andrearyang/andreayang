d6650f3bb636c8fa5fa668dd67354622403f74af
Andrea Yang <andreayang64@gmail.com>
2024-09-08 11:42:43 -0700

    styling

--- components/nav.js
+++ components/nav.js
@@ -1,47 +1,43 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { CgDarkMode } from 'react-icons/cg'
-import {useTheme} from 'next-themes'
+import Link from 'next/link';
+import { CgDarkMode } from 'react-icons/cg';
+import { useTheme } from 'next-themes';
 
 const links = [
-    { label: "Projects", href: "/" },
-    { label: "Writing", href: "/" },
-    { label: "Resume", href: "/" },
-]
+  { label: "Projects", href: "/" },
+  { label: "Writing", href: "/" },
+  { label: "Resume", href: "/" },
+];
 
 export default function Nav() {
-    const {theme, setTheme} = useTheme()
-    useEffect(() => {
-        console.log('hello')
-    })
-    return (
-        <nav className="dark:text-white">
-            <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-8 mt-6 sm:mt-0">
-                <li>Andrea Yang</li>
-                <ul className={`mx-auto sm:mx-0 flex flex-row space-x-5`}>
-                    {links.map(({ href, label }) => (
-                        <li className="self-center" key={`${href}${label}`}>
-                            <Link
-                                href={href}
-                                className={`font-inter px-4 py-2 rounded hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10`}>
+  const { theme, setTheme } = useTheme();
 
-                                {label}
-
-                            </Link>
-                        </li>
-                    ))}
-                    <li>
-                        <button
-                            onClick={() => {
-                                setTheme(theme === 'dark' ? 'light' : 'dark');
-                                document.querySelector("#theme_toggle").classList.toggle("rotate-180");
-                            }}
-                            className="p-2 rounded-full hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 transform duration-200" id="theme_toggle">
-                            <CgDarkMode size={24} />
-                        </button>
-                    </li>
-                </ul>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="dark:text-white">
+      <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-8 mt-6 sm:mt-0">
+        <li>Andrea Yang</li>
+        <ul className="mx-auto sm:mx-0 flex flex-row space-x-5">
+          {links.map(({ href, label }) => (
+            <li className="self-center" key={`${href}${label}`}>
+              <Link
+                href={href}
+                className="font-inter px-4 py-2 rounded hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10"
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <button
+              onClick={() => {
+                setTheme(theme === 'dark' ? 'light' : 'dark');
+              }}
+              className="p-2 rounded-full hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 transform duration-200"
+            >
+              <CgDarkMode size={24} />
+            </button>
+          </li>
+        </ul>
+      </ul>
+    </nav>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,12 @@
 import HeadObject from '../components/head'
 import Nav from '../components/nav';
+import { useTheme } from 'next-themes';
 
 export default function Home() {
-  const [theme, setTheme] = useState(null);
+  const {theme, setTheme} = useTheme();
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if(storedTheme) {
-      document.documentElement.classList.add(storedTheme);
-      setTheme(storedTheme);
-    } else {
-      const defaultTheme = 'light';
-      document.documentElement.classList.add(defaultTheme);
-      setTheme(defaultTheme);
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'dark'? 'light' : 'dark';
-    document.documentElement.classList.remove(theme);
-    document.documentElement.classList.add(newTheme);
-    localStorage.setItem('theme', newTheme);
-    setTheme(newTheme);
-  }
-  
   return (
-    <div className="dark:text-white dark:bg-black">
+    <div className={`${theme === 'dark' ? 'dark' : ' '}`}>
       <HeadObject>
         {/* You can put extra tags in here, or leave it blank */}
       </HeadObject>
@@ -33,7 +14,7 @@ export default function Home() {
       <div class="flex flex-row p-12 text-light-text font-bold text-7xl">
         <div class="flex-1 flex flex-col ml-5 mr-55">
           <h1 className="flex-1">Hi! I'm Andrea.</h1> <br></br>
-          <div class="font-normal text-white text-xl">
+          <div class="font-normal text-black dark:text-white text-xl">
             <h2>I'm a 16 year old hacker from California. I like computers and cells. </h2>
             <br></br>
             <h2>Acessible education is really important to me. I also strongly believe in pursuing math and making as an art. </h2>
@@ -41,7 +22,7 @@ export default function Home() {
             <br></br>
             <h3>Outside of that, I'm an ardent lover of Studio Ghibli, Stardew Valley, Nintendo, and boba. I find any and all things fascinating, and I'm always happy to share a good conversation with someone. </h3>
           </div>
-          <div class="font-semibold text-light-light text-3xl mt-12">
+          <div class="font-semibold text-dark-green dark:text-light-light text-3xl mt-12">
             <h1>see you around!</h1>
           </div>
         </div>
